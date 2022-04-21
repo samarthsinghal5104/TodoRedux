3fd3cb8790781fe4b3a472419449598eb47965bf
Samarth Singhal <sasinghal@microsoft.com>
2022-04-21 17:17:12 +0530

    removing persistent state

--- src/store.js
+++ src/store.js
@@ -1,15 +1,15 @@
-// import { createStore, combineReducers, applyMiddleware } from 'redux';
-// import { persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-// import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
-// import thunk from 'redux-thunk';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-// import { todos, isLoading } from './todos/reducers';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { todos, isLoading } from "./todos/reducers";
 
-// const reducers = {
-//     todos,
-//     isLoading,
-// };
+const reducers = {
+  todos,
+  isLoading,
+};
 
 // const persistConfig = {
 //     key: 'root',
@@ -17,25 +17,12 @@
 //     stateReconciler: autoMergeLevel2,
 // }
 
-// const rootReducer = combineReducers(reducers);
+const rootReducer = combineReducers(reducers);
 // const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// export const configureStore = () =>
-//     createStore(
-//         persistedReducer,
-//         composeWithDevTools(
-//             applyMiddleware(thunk)
-//         )
-//     );
-
-import { createStore, combineReducers } from "redux";
-import { todos,isLoading } from "./todos/reducers";
-
-const reducers = {
-  todos,
-  isLoading,
-};
-
-const rootReducer = combineReducers(reducers);
-
-export const configureStore = () => createStore(rootReducer);
+export const configureStore = () =>
+  createStore(
+    rootReducer,
+    // persistedReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+  );
